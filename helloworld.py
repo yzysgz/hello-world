be1e5e860f6eb47a616979ca0146135c698c8ad0
ds <ds942846016@163.com>
2019-08-23 10:59:08 +0800

    recovery3

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,6 @@
-"""注释"""
+"""注释---"""
 def main():
-<<<<<<< HEAD
     print("hello world!")
 
 if __name__ == "__main__":
     main()
-=======
-    print("hello world!")
->>>>>>> parent of e3b2a28... add if __name__ == "_main_":
